ec43db78c5f1e38e27c3c5454b4f1f1214fe2aca
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-06-23 14:45:26 +0100

    card now accents with theme color

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Image from "./UI/Image";
-import Container from "./UI/Container";
-import { Heading, Paragraph } from "./UI/Text";
+import { Caption, Paragraph } from "./UI/Text";
 
 const ProjectCard = ({themeColor}) => {
+
     return (
-        <div className='transition-all duration-700 bg-space-500 border border-space-400 rounded-xl shadow-card relative aspect-[9/16] h-[max(80vmin,45rem)] lg:h-[max(40vmin,40rem)] overflow-hidden flex flex-col'>
+        <div className='transition-all duration-300 bg-space-500 border border-space-400 rounded-xl shadow-card relative aspect-[9/16] h-[max(80vmin,45rem)] lg:h-[max(40vmin,40rem)] overflow-hidden flex flex-col hover:scale-105'>
             <Image src="https://dummyimage.com/300x300/cfcfcf/000000" className='h-1/2 w-full object-cover object-center border-b border-space-500'/>
             <div className='grow p-6'>
-                <Heading className='text-2xl md:text-3xl'>
+                <Caption themeColor={{theme: themeColor, color: 'green'}} className='text-lg md:text-xl'>
                     Project
-                </Heading>
+                </Caption>
                 <Paragraph>
                     Project information. Quick overview of the project goes here.
                 </Paragraph>
