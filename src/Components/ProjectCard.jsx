1c0d7c8ec7342ba61f86d2fc650bcdb58a692765
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-03 16:54:17 +0100

    update card responsiveness and sizing

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -5,9 +5,9 @@ import { Caption, Paragraph } from "./UI/Text";
 const ProjectCard = ({themeColor}) => {
 
     return (
-        <div className='transition-all duration-300 bg-space-500 border border-space-400 rounded-xl shadow-card relative aspect-[9/16] h-[max(80vmin,45rem)] lg:h-[max(40vmin,40rem)] overflow-hidden flex flex-col hover:scale-105'>
-            <Image src="https://dummyimage.com/300x300/cfcfcf/000000" className='h-1/2 w-full object-cover object-center border-b border-space-500'/>
-            <div className='grow p-6'>
+        <div className='transition-all duration-300 bg-space-500 border border-space-400 rounded-xl shadow-card relative lg:aspect-[9/16] lg:h-[max(80vmin,25rem)] lg:h-[max(80vmin,25rem)] overflow-hidden flex lg:flex-col hover:scale-105'>
+            <Image src="https://dummyimage.com/300x300/cfcfcf/000000" className='h-full lg:h-1/2 w-1/2 lg:w-full object-cover object-center border-b border-space-500'/>
+            <div className='my-auto lg:my-0 grow p-6'>
                 <Caption themeColor={{theme: themeColor, color: 'green'}} className='text-lg md:text-xl'>
                     Project
                 </Caption>
