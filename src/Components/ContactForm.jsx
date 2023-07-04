b1a6985bb4934247d865d4e0a0c59f4443276eaa
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-04 14:19:32 +0100

    Added functionality to contact from

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -3,26 +3,56 @@ import Button from "./UI/Button";
 import TextBox from "./UI/TextBox";
 import Label from "./UI/Label";
 import TextArea from "./UI/TextArea";
+import { Caption, Heading, Paragraph } from "./UI/Text";
+import { useForm } from 'react-hook-form';
+import { sendEmail, contactFromValidation } from "../Library/Utils";
+import { zodResolver } from '@hookform/resolvers/zod';
 
 const ContactForm = ({themeColor}) => {
 
-    const handleSubmit = () => {
-        console.log('Submit detected');
+    const {register, handleSubmit, formState: { errors, isSubmitSuccessful, isSubmitting } } = useForm({resolver: zodResolver(contactFromValidation), mode:'onTouched'});
+    console.log(errors, isSubmitSuccessful, isSubmitting);
+    const onSubmit = async (data) => {
+        console.log(data);
+        await sendEmail(data);
     };
 
-    return (
-        <form onSubmit={handleSubmit()} className='flex flex-col gap-y-6'>
+    return isSubmitSuccessful ? (
+            <div>
+                <Caption themeColor={themeColor}>Read you loud and clear!</Caption>
+                <Heading>I will contact you soon</Heading>
+            </div>
+        ) : (
+        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-y-6'>
             <div>
                 <Label htmlFor='name'>Name</Label>
-                <TextBox id='name' themeColor={themeColor}/>
+                <TextBox 
+                    id='name'
+                    className={errors.name ? 'border-neon-red' : ''}
+                    {...register('name', {required:true})}
+                    themeColor={themeColor}
+                />
+                {errors.name && (<Paragraph>{errors.name.message}</Paragraph>)}
             </div>
             <div>
                 <Label htmlFor='email'>E-Mail</Label>
-                <TextBox id='email' themeColor={themeColor}/>
+                <TextBox 
+                    id='email'
+                    className={errors.email ? 'border-neon-red' : ''}
+                    {...register('email', {required:true})}
+                    themeColor={themeColor}
+                />
+                {errors.email && (<Paragraph>{errors.email.message}</Paragraph>)}
             </div>
             <div>
                 <Label htmlFor='message'>Message</Label>
-                <TextArea id='message' themeColor={themeColor}/>
+                <TextArea 
+                    id='message'
+                    className={errors.message ? 'border-neon-red' : ''}
+                    {...register('message', {required:true})}
+                    themeColor={themeColor}
+                />
+                {errors.message && (<Paragraph>{errors.message.message}</Paragraph>)}
             </div>
             <Button theme='blue' size='medium'>Send</Button>
         </form>
--- src/Library/Utils.js
+++ src/Library/Utils.js
@@ -1,5 +1,7 @@
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import emailjs from '@emailjs/browser';
+import { z } from 'zod';
 
 export const reduceClasses = (...inputs) => {
     return twMerge(clsx(inputs));
@@ -27,3 +29,19 @@ export const siteLinks = (...inputs) => {
         return inputs.includes(link.label.toLocaleLowerCase());
     }): primaryLinks;
 }
+
+export const sendEmail = async (data) => {
+    data.source = 'www.sardonxy-studios.com';
+    return emailjs.send('service_3fwt3bq', 'template_g6j4jcs', data, 'P590nBXSqdIgcY0qZ')
+    .then((result) => {
+        console.log(result);
+    }, (error) => {
+        console.log(error);
+    });
+}
+
+export const contactFromValidation = z.object({
+    name: z.string().nonempty({message: "Name cannot be empty"}).max(60, {message: "Name must not be more than 60 characters"}),
+    email: z.string().email(),
+    message: z.string().nonempty({ message: "Message cannot be empty" }).max(1000, {message: "Message cannot be more that 1000 characters"})
+});
