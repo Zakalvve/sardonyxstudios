54051a36e51a8d407281b9ac84ea26a2bbd364ba
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-06 14:41:25 +0100

    first work on mobile nav bar

---
+++ src/Components/Layout/MobileNav.jsx
@@ -0,0 +1,206 @@
+
+import React, { useEffect } from "react";
+import Container from "../UI/Container";
+import { reduceClasses } from "../../Library/Utils";
+import { useLockedBody } from "usehooks-ts";
+
+import { AnimatePresence, motion, useAnimate } from "framer-motion";
+
+const MobileNav = ({isOpen, toggleOpen, links, height}) => {
+    useLockedBody(isOpen);
+    const bgVariants = {
+        open: {
+            opacity: 1,
+            transition: { duration: 0.3 }
+        },
+        closed: {
+            opacity: 0,
+            transition: { duration: 0.3 }
+        }
+    }
+    const navVariants = {
+        open: {
+          transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+        },
+        closed: {
+          transition: { staggerChildren: 0.05, staggerDirection: -1 }
+        }
+    };
+
+    const itemVariants = {
+        open: {
+          y: 0,
+          opacity: 1,
+          transition: {
+            y: { stiffness: 1000, velocity: -100 }
+          }
+        },
+        closed: {
+          y: 30,
+          opacity: 0,
+          transition: {
+            y: { stiffness: 1000 }
+          }
+        }
+    };
+
+    return (
+        <AnimatePresence>
+            {isOpen && (
+                <motion.div
+                    initial={bgVariants.closed}
+                    animate={bgVariants.open}
+                    exit={bgVariants.closed}
+                    variants={bgVariants}
+                    aria-controls="mobile-navigation-toggle"
+                    className='border-1 p-4 border-space-400 bg-space-600/90 backdrop-blur-sm supports-[backdrop-filter]:bg-space-600/80 min-w-[10rem] w-[35vw]'
+                >
+                <Container>
+                    <nav
+                        className='flex flex-col justify-center'
+                    >
+                        <motion.ul>
+                            {links.map((link, i) => (
+                                    <motion.li 
+                                        key={i}
+                                        initial={itemVariants.closed}
+                                        animate={itemVariants.open}
+                                        exit={itemVariants.closed}
+                                        whileHover={{ scale: 1.1 }}
+                                        whileTap={{ scale:0.95 }}
+                                        onClick={toggleOpen}
+                                        className='py-2'
+                                    >
+                                        <a
+                                            href={link.href}
+                                            className={reduceClasses('uppercase transition-[letter-spacing,color] text-white', 'focus-visible:underline hover:underline')}
+                                        >
+                                            {link.label}
+                                        </a>
+                                    </motion.li>
+                                ))}
+                        </motion.ul>
+                    </nav>
+                </Container>
+            </motion.div>
+        )}
+        </AnimatePresence>
+    );
+}
+
+const MobileNavOperlay = ({ isOpen, toggleOpen,}) => {    
+    return (
+        <AnimatePresence>
+            {isOpen && (
+                <motion.div 
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 0.3 }}
+                    aria-hidden
+                    onClick={toggleOpen}
+                    className="fixed inset-0 bg-space-600/40 -z-10 backdrop-blur-sm"
+                />
+            )}
+        </AnimatePresence>
+    );
+}
+
+const MobileNavToggle = ({isOpen, toggleOpen}) => {
+    const [scope, animate] = useAnimate();
+
+    useEffect(() => {
+        animate([
+          [
+            'path[data-toggle-bar-top]',
+            {
+              y: isOpen
+                ? ['0rem', '-0.325rem', '-0.325rem']
+                : ['-0.325rem', '-0.325rem', '0rem'],
+              d: isOpen
+                ? [
+                    'M3,17.25C3,16.839 3.339,16.5 3.75,16.5L20.25,16.5C20.661,16.5 21,16.839 21,17.25C21,17.661 20.661,18 20.25,18L3.75,18C3.339,18 3,17.661 3,17.25Z',
+                    'M3,17.25C3,16.839 3.339,16.5 3.75,16.5L20.25,16.5C20.661,16.5 21,16.839 21,17.25C21,17.661 20.661,18 20.25,18L3.75,18C3.339,18 3,17.661 3,17.25Z',
+                    'M5.636,10.886C5.927,10.595 6.406,10.595 6.697,10.886L18.364,22.553C18.655,22.844 18.655,23.323 18.364,23.614C18.073,23.905 17.594,23.905 17.303,23.614L5.636,11.947C5.345,11.656 5.345,11.177 5.636,10.886Z',
+                  ]
+                : [
+                    'M5.636,10.886C5.927,10.595 6.406,10.595 6.697,10.886L18.364,22.553C18.655,22.844 18.655,23.323 18.364,23.614C18.073,23.905 17.594,23.905 17.303,23.614L5.636,11.947C5.345,11.656 5.345,11.177 5.636,10.886Z',
+                    'M3,17.25C3,16.839 3.339,16.5 3.75,16.5L20.25,16.5C20.661,16.5 21,16.839 21,17.25C21,17.661 20.661,18 20.25,18L3.75,18C3.339,18 3,17.661 3,17.25Z',
+                    'M3,17.25C3,16.839 3.339,16.5 3.75,16.5L20.25,16.5C20.661,16.5 21,16.839 21,17.25C21,17.661 20.661,18 20.25,18L3.75,18C3.339,18 3,17.661 3,17.25Z',
+                  ],
+            },
+            { duration: 0.3 },
+          ],
+          [
+            'path[data-toggle-bar-middle]',
+            { opacity: isOpen ? [1, 1, 0] : [0, 0, 1] },
+            { at: '<', duration: 0.3 },
+          ],
+          [
+            'path[data-toggle-bar-bottom]',
+            {
+              y: isOpen
+                ? ['0rem', '0.325rem', '0.325rem']
+                : ['0.325rem', '0.325rem', '0rem'],
+              d: isOpen
+                ? [
+                    'M3,6.75C3,6.339 3.339,6 3.75,6L20.25,6C20.661,6 21,6.339 21,6.75C21,7.161 20.661,7.5 20.25,7.5L3.75,7.5C3.339,7.5 3,7.161 3,6.75Z',
+                    'M3,6.75C3,6.339 3.339,6 3.75,6L20.25,6C20.661,6 21,6.339 21,6.75C21,7.161 20.661,7.5 20.25,7.5L3.75,7.5C3.339,7.5 3,7.161 3,6.75Z',
+                    'M5.636,13.114C5.345,12.823 5.345,12.344 5.636,12.053L17.303,0.386C17.594,0.095 18.073,0.095 18.364,0.386C18.655,0.677 18.655,1.156 18.364,1.447L6.697,13.114C6.406,13.405 5.927,13.405 5.636,13.114Z',
+                  ]
+                : [
+                    'M5.636,13.114C5.345,12.823 5.345,12.344 5.636,12.053L17.303,0.386C17.594,0.095 18.073,0.095 18.364,0.386C18.655,0.677 18.655,1.156 18.364,1.447L6.697,13.114C6.406,13.405 5.927,13.405 5.636,13.114Z',
+                    'M3,6.75C3,6.339 3.339,6 3.75,6L20.25,6C20.661,6 21,6.339 21,6.75C21,7.161 20.661,7.5 20.25,7.5L3.75,7.5C3.339,7.5 3,7.161 3,6.75Z',
+                    'M3,6.75C3,6.339 3.339,6 3.75,6L20.25,6C20.661,6 21,6.339 21,6.75C21,7.161 20.661,7.5 20.25,7.5L3.75,7.5C3.339,7.5 3,7.161 3,6.75Z',
+                  ],
+            },
+            { at: '<', duration: 0.3 },
+          ],
+        ]);
+      }, [isOpen, animate]);
+
+      return (
+        <button
+            type='button'
+            id='mobile-nav-toggle'
+            aria-haspopup='menu'
+            aria-expanded={isOpen}
+            aria-label='Toggle navigation menu'
+            onClick={toggleOpen}
+        >
+            <span className='sr-only'>Menu</span>
+            <motion.svg
+                ref={scope}
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                aria-hidden
+                className="h-7 w-7"
+            >
+                <motion.path
+                fillRule="evenodd"
+                d="M3,17.25C3,16.839 3.339,16.5 3.75,16.5L20.25,16.5C20.661,16.5 21,16.839 21,17.25C21,17.661 20.661,18 20.25,18L3.75,18C3.339,18 3,17.661 3,17.25Z"
+                clipRule="evenodd"
+                data-toggle-bar-top
+                />
+                <motion.path
+                fillRule="evenodd"
+                d="M3,12C3,11.589 3.339,11.25 3.75,11.25L20.25,11.25C20.661,11.25 21,11.589 21,12C21,12.411 20.661,12.75 20.25,12.75L3.75,12.75C3.339,12.75 3,12.411 3,12Z"
+                clipRule="evenodd"
+                data-toggle-bar-middle
+                />
+                <motion.path
+                fillRule="evenodd"
+                d="M3,6.75C3,6.339 3.339,6 3.75,6L20.25,6C20.661,6 21,6.339 21,6.75C21,7.161 20.661,7.5 20.25,7.5L3.75,7.5C3.339,7.5 3,7.161 3,6.75Z"
+                clipRule="evenodd"
+                data-toggle-bar-bottom
+                />
+            </motion.svg>
+        </button>
+      );
+}
+
+MobileNav.Overlay = MobileNavOperlay;
+MobileNav.Toggle = MobileNavToggle;
+
+export default MobileNav;
--- src/Components/Layout/NavBar.jsx
+++ src/Components/Layout/NavBar.jsx
@@ -1,53 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import useScrollPosition from '@react-hook/window-scroll';
 import { useWindowHeight, } from '@react-hook/window-size'
 import { reduceClasses, siteLinks } from "../../Library/Utils";
 import Container from "../UI/Container";
 import ButtonLink from "../UI/ButtonLink";
+import MobileNav from "./MobileNav";
 
 const NavBar = ({isBackgroundTranhsparent = true}) => {
     const windowHeight = useWindowHeight();
     const scrollPos = useScrollPosition(60);
 
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    }
+
     const scrolledDownOnePage = scrollPos > windowHeight - 1;
-    const isBackgroundBlurred = !isBackgroundTranhsparent || scrolledDownOnePage;
+    const isBackgroundBlurred = !isBackgroundTranhsparent || scrolledDownOnePage || isOpen;
 
     const links = siteLinks('about', 'projects');
 
     return (
-        <header aria-label="Primary" className="fixed top-0 z-40 w-full">
+        <header aria-label="Primary" className="fixed flex flex-col top-0 z-40 w-full">
             <div className={reduceClasses(
-                'border-b transition-colors duration-500 py-2',
-                isBackgroundBlurred ? 'border-space-300 bg-space-600/90 backdrop-blur-md supports-[backdrop-filter]:bg-space-600/50'
+                'border-b transition-colors duration-5000 py-2',
+                isBackgroundBlurred ? 'border-space-300 bg-space-600/80 backdrop-blur-md supports-[backdrop-filter]:bg-space-600/50'
                                     : 'border-transparent bg-transparent'
             )}>
                 <Container>
                     <div className="grid grid-cols-3">
                         <div className="flex items-center md:hidden">
-                            Mobile Nav Button
+                            <MobileNav.Toggle isOpen={isOpen} toggleOpen={toggle}/>
                         </div>
                         <nav aria-label="Primary" className="hidden items-center gap-x-6 md:flex">
-                            {links.map((link, index) => 
-                                <a
-                                    key={index}
-                                    href={link.href}
-                                    className="relative underline underline-offset-4 decoration-transparent py-1 text-sm uppercase text-white transition-all duration-300 hover:decoration-white"
-                                >
-                                    {link.label}
-                                </a>
-                            )}
+                            <ul className="hidden items-center gap-x-6 md:flex">
+                                {links.map((link, index) =>
+                                    <li className='relative py-1'><a
+                                        key={index}
+                                        href={link.href}
+                                        className="underline underline-offset-4 decoration-transparent text-sm uppercase text-white transition-all duration-300 hover:decoration-white"
+                                    >
+                                        {link.label}
+                                    </a></li>
+                                )}
+                            </ul>
                         </nav>
                         <div className="flex items-center justify-center">
                             Logo
                         </div>
                         <div className="flex items-center justify-end">
-                            <ButtonLink  href='/#contact' theme='primary' size='small' className='bg-transparent hover:bg-transparent hover:border-transparent'>
+                            <ButtonLink  href='/#contact' theme='primary' size='small' onClick={() => setIsOpen(false)} className='bg-transparent hover:bg-transparent hover:border-transparent'>
                                 Contact
                             </ButtonLink>
                         </div>
                     </div>
                 </Container>
             </div>
+            <MobileNav isOpen={isOpen} toggleOpen={toggle} links={links} height={windowHeight}/>
+            <MobileNav.Overlay isOpen={isOpen} toggleOpen={toggle}/>
         </header>
     );
 };
