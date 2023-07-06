9729f087b4c991ae4f55f9e6388e0c43e6464cbf
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-06 09:56:20 +0100

    slight change to card code layout

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -6,9 +6,16 @@ import { Link } from "react-router-dom";
 const ProjectCard = ({themeColor, project}) => {
     return (
         <Link to={`projects/${project.id}`}>
-            <div className='bg-space-500 border border-space-400 rounded-xl shadow-card relative flex flex-col h-[max(70vh,27rem)] max-h-[40rem] sm:w-[max(30vw,23rem)] transition-all duration-300 hover:scale-105 overflow-hidden'>
+            <div className='bg-space-500 border border-space-400 rounded-xl shadow-card relative flex flex-col
+
+                    h-[max(70vh,27rem)]
+                    sm:w-[max(30vw,23rem)]
+                    max-h-[40rem]
+
+                    transition-all duration-300 hover:scale-105 overflow-hidden'
+            >
                     <Image src={project.image} className='h-1/2 w-full object-cover border-b border-space-500'/>
-                    <div className='my-auto lg:my-0 grow p-6 max-h-1/2'>
+                    <div className='my-auto grow p-6 max-h-1/2'>
                         <Caption themeColor={themeColor} className='text-lg md:text-xl'>
                             {project.name}
                         </Caption>
