b361e01b385f890913b916ea9a4ab807f8568004
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-10 15:03:08 +0100

    final tweaks to project card

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -16,9 +16,9 @@ function formatDate(input) {
 
 const ProjectCard = ({themeColor, project}) => {
     return (
-        <Link to={`projects/${project.id}`} className='relative rounded-lg flex-1 border-space-400 border bg-space-500 shadow-card duration-300 md:hover:scale-[1.03] overflow-hidden md:basis-[45vmin] min-w-[35vw]'
+        <Link to={`projects/${project.id}`} className='relative rounded-lg flex-1 border-space-400 border bg-space-500 shadow-card duration-300 md:hover:scale-[1.03] overflow-hidden md:basis-[30vw]'
         >
-                    <Image src={project.image} className='w-full aspect-video object-cover object-centerborder-b border-space-400'/>
+                    <Image src={project.image} className='w-full aspect-video object-cover object-centerborder-b border-space-400 max-h-[70%]'/>
                     <div className='grow py-2 md:py-4 px-4 md:px-8 flex flex-col gap-y-2'>
                         <p className="text-sm text-space-100">{formatDate(project.date)}</p>
                         <Caption themeColor={themeColor} className='text-lg md:text-xl mb-0'>
@@ -29,7 +29,7 @@ const ProjectCard = ({themeColor, project}) => {
                                 {project.skills.join(', ')}
                             </Paragraph>
                         )}
-                        <Paragraph >
+                        <Paragraph className='max-w-full'>
                             {project.description}
                         </Paragraph>
                     </div>
