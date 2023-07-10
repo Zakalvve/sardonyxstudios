170128b182b22ed8e41a224237c62c39ff9987c9
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-10 13:38:32 +0100

    further changes to card

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -5,10 +5,10 @@ import { Link } from "react-router-dom";
 
 const ProjectCard = ({themeColor, project}) => {
     return (
-        <Link to={`projects/${project.id}`} className='relative p-4 md:p-8 rounded-lg flex-1 border-space-400 border bg-gradient-radial from-space-600 to-space-500 md:hover:-translate-y-2 transition-[transform,filter] shadow-card duration-300 md:hover:scale-[1.01] focus-visible:-translate-y-2 focus-visible:drop-shadow-lg focus-visible:scale-[1.01] flex-grow'
+        <Link to={`projects/${project.id}`} className='relative rounded-lg flex-1 border-space-400 border bg-gradient-radial from-space-600 to-space-500 shadow-card duration-300 md:hover:scale-[1.05]'
         >
-                    <Image src={project.image} className='h-1/2 w-full object-cover border-b border-space-500'/>
-                    <div className='my-auto grow p-6 max-h-1/2'>
+                    <Image src={project.image} className='h-1/2 w-full object-cover border-b border-space-400'/>
+                    <div className='my-auto grow p-4 md:p-8 max-h-1/2'>
                         <Caption themeColor={themeColor} className='text-lg md:text-xl'>
                             {project.name}
                         </Caption>
