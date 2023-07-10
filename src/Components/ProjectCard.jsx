a1b3a729d964fedde45fe137da2a86c71b7cfc04
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-10 13:20:48 +0100

    first revision to cards

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -5,15 +5,8 @@ import { Link } from "react-router-dom";
 
 const ProjectCard = ({themeColor, project}) => {
     return (
-        <Link to={`projects/${project.id}`}>
-            <div className='bg-space-500 border border-space-400 rounded-xl shadow-card relative flex flex-col
-
-                    h-[max(70vh,27rem)]
-                    sm:w-[max(30vw,23rem)]
-                    max-h-[40rem]
-
-                    transition-all duration-300 hover:scale-105 overflow-hidden'
-            >
+        <Link to={`projects/${project.id}`} className='relative p-4 md:p-8 rounded-lg flex-1 border-space-400 border bg-gradient-radial from-space-600 to-space-500 md:hover:-translate-y-2 transition-[transform,filter] shadow-card duration-300 md:hover:scale-[1.01] focus-visible:-translate-y-2 focus-visible:drop-shadow-lg focus-visible:scale-[1.01] flex-grow'
+        >
                     <Image src={project.image} className='h-1/2 w-full object-cover border-b border-space-500'/>
                     <div className='my-auto grow p-6 max-h-1/2'>
                         <Caption themeColor={themeColor} className='text-lg md:text-xl'>
@@ -23,9 +16,17 @@ const ProjectCard = ({themeColor, project}) => {
                             {project.description}
                         </Paragraph>
                     </div>
-            </div>
         </Link>
     );
 }
 
 export default ProjectCard;
+
+/*
+bg-space-500 border border-space-400 rounded-xl shadow-card relative flex flex-col
+
+            h-[max(70vh,27rem)]
+            sm:w-[max(30vw,23rem)]
+            max-h-[40rem]
+
+            transition-all duration-300 hover:scale-105 overflow-hidden*/
--- src/Components/ProjectCards.jsx
+++ src/Components/ProjectCards.jsx
@@ -2,7 +2,7 @@ import ProjectCard
  from "./ProjectCard";
 const ProjectCards = ({themeColor, projects}) => {
     return (
-        <div className='flex flex-nowrap flex-col md:flex-row md:flex-wrap items-center justify-center gap-[max(4vmin,1rem)] py-8 transition-all'>
+        <div className='max-md:flex-col flex gap-4 md:gap-8 mt-4 transition-all'>
             {projects.map((project) => {
                 return (<ProjectCard key={project.id} themeColor={themeColor} project={project}/>)
             })}
