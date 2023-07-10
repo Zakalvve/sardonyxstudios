7edc9aebb31b7393e2b09f86df896617d90841ff
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-10 16:59:52 +0100

    added skills icons to project card

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "./UI/Image";
 import { Caption, Paragraph } from "./UI/Text";
 import { Link } from "react-router-dom";
+import SkillsList from "./SkillsList";
 
 function formatDate(input) {
     const date = new Date(input);
@@ -18,20 +19,16 @@ const ProjectCard = ({themeColor, project}) => {
     return (
         <Link to={`projects/${project.id}`} className='relative rounded-lg flex-1 border-space-400 border bg-space-500 shadow-card duration-300 md:hover:scale-[1.03] overflow-hidden md:basis-[30vw]'
         >
-                    <Image src={project.image} className='w-full aspect-video object-cover object-centerborder-b border-space-400 max-h-[70%]'/>
-                    <div className='grow py-2 md:py-4 px-4 md:px-8 flex flex-col gap-y-2'>
+                    <Image src={project.image} className='w-full aspect-video object-cover object-centerborder-b border-space-400'/>
+                    <div className='grow py-4 px-4 md:px-8 flex flex-col gap-y-2 md:gap-y-4'>
                         <p className="text-sm text-space-100">{formatDate(project.date)}</p>
                         <Caption themeColor={themeColor} className='text-lg md:text-xl mb-0'>
                             {project.name}
                         </Caption>
-                        {project.skills && (
-                            <Paragraph className='text-sm mb-2'>
-                                {project.skills.join(', ')}
-                            </Paragraph>
-                        )}
                         <Paragraph className='max-w-full'>
                             {project.description}
                         </Paragraph>
+                        <SkillsList skills={project.skills}/>
                     </div>
         </Link>
     );
---
+++ src/Components/SkillsList.jsx
@@ -0,0 +1,15 @@
+import { icons } from "../Assets/assets"
+
+const SkillsList = ({skills}) => {
+    const skillsIcons = icons.SkillsIcons.filter(skill => skills.find(skillString => skillString.toLowerCase() === skill.name)).map(skill => skill.icon);
+
+    return (
+        <div className='flex gap-x-3'>
+            {skillsIcons.map((Icon, i) => (
+                <Icon key={i} className='h-8 w-8'/>
+            ))}
+        </div>
+    )
+}
+
+export default SkillsList;
