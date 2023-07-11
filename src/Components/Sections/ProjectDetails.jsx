de0229d5a1d573418b6bba983a2e919dfea7e74f
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-11 14:15:17 +0100

    added skill pills and back button

--- src/Components/Sections/ProjectDetails.jsx
+++ src/Components/Sections/ProjectDetails.jsx
@@ -5,8 +5,15 @@ import { Heading, Date, Paragraph, Caption} from '../UI/Text';
 import ProjectCards from '../ProjectCards'
 import { projectsData } from '../../Data/data';
 import { shuffleArray } from '../../Library/Utils';
+import Pill from '../UI/Pill';
+import { useNavigate } from 'react-router-dom';
 
 const ProjectDetails = ({project}) => {
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate(-1);
+    };
 
     return (
         <>
@@ -15,8 +22,13 @@ const ProjectDetails = ({project}) => {
                 <Container>
                     <div className='relative z-10 flex h-full flex-col justify-end gap-4'>
                         <Date date={project.date} />
-                        <Heading>{project.name}</Heading>
-                        <button className='flex items-center hover:opacity-80 focus-visible:opacity-80 transition-opacity group'>
+                        <Heading className='mb-0'>{project.name}</Heading>
+                        <div className='flex flex-row gap-2'>
+                            {project.skills.map((skill, i) => (
+                                <Pill key={i} className='bg-neon-green text-space-500'>{skill}</Pill>
+                            ))}
+                        </div>
+                        <button onClick={handleBack} className='mt-4 flex items-center hover:opacity-80 focus-visible:opacity-80 transition-opacity group'>
                             <icons.BackArrow className='mr-2 h-12 w-12 transition-transform duration-300 group-hover:-translate-x-1 group-focus-visible:-translate-x-1 lg:h-7 lg:w-7"'/>
                             Go Back
                         </button>
@@ -40,7 +52,7 @@ const ProjectDetails = ({project}) => {
             <section className='py-20 bg-space-600'>
                 <Container>
                     <div className='flex flex-col items-center text-center'>
-                        <Caption themeColor='neon-green' className='mb-8'>Other Projects</Caption>
+                        <Caption themeColor='neon-red' className='mb-8'>Other Projects</Caption>
                         <ProjectCards projects={shuffleArray(projectsData).slice(0,2)}/>
                     </div>
                 </Container>
