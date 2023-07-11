3202458b082624e7c1355df01be77366e8fa5a6f
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-11 16:58:34 +0100

    added sopme space around skills icons

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -9,7 +9,7 @@ const ProjectCard = ({themeColor, project}) => {
         <Link to={`/projects/${project.id}`} className='relative rounded-lg flex flex-col flex-1 border-space-400 border bg-space-500 shadow-card duration-300 md:hover:scale-[1.03] overflow-hidden md:basis-[30vw] 2xl:basis-[20vw]'
         >
                     <Image src={project.image} className='w-full aspect-video object-cover object-centerborder-b border-space-400'/>
-                    <div className='grow py-4 px-4 md:px-8 flex flex-col gap-y-2 md:gap-y-4'>
+                    <div className='grow py-6 px-4 md:px-8 flex flex-col gap-y-2 md:gap-y-4'>
                         <Date date={project.date}/>
                         <Caption themeColor={themeColor} className='text-lg md:text-xl mb-0'>
                             {project.name}
@@ -17,7 +17,7 @@ const ProjectCard = ({themeColor, project}) => {
                         <Paragraph className='max-w-full'>
                             {project.description}
                         </Paragraph>
-                        <div className='flex-1 flex flex-col-reverse'>
+                        <div className='flex-1 flex flex-col-reverse mt-2'>
                             <SkillsList skills={project.skills}/>
                         </div>
                     </div>
