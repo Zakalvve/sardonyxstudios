10682cb5e3cb61fad6b50a859f21975af2cd1ad6
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-11 16:58:53 +0100

    rewoked layout of about section

--- src/Components/Sections/About.jsx
+++ src/Components/Sections/About.jsx
@@ -7,14 +7,9 @@ import { images } from '../../Assets/assets'
 const About = ({themeColor}) => {
     const itsMe = images.MeOnGuitarImage;
     return (
-        <section id='about' className='bg-space-600 relative z-10 flex min-h-screen w-full flex-col-reverse items-center lg:flex-row mb-[-2px]'>
-            <Image 
-                className='inset-0 max-h-screen w-full object-cover object-center lg:absolute lg:h-full lg:w-1/2' 
-                src={itsMe}
-                loading='lazy'
-            />
+        <section id='about' className='bg-space-600 relative z-10 flex lg:min-h-screen w-full flex-col items-center lg:flex-row-reverse mb-[-2px]'>
             <Container>
-                <div className='w-full py-28 lg:flex'>
+                <div className='w-full py-20 lg:flex'>
                     <div className='lg:ml-auto lg:basis-1/2 lg:pl-10 xl:pl-20'>
                         <Caption themeColor={themeColor}>About</Caption>
                         <Heading>
@@ -26,6 +21,11 @@ const About = ({themeColor}) => {
                     </div>
                 </div>
             </Container>
+            <Image 
+                className='inset-0 max-h-screen w-full object-cover object-center lg:absolute lg:h-full lg:w-1/2' 
+                src={itsMe}
+                loading='lazy'
+            />
         </section>
     );
 }
