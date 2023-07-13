d81a4af4c6b1e2e6ffc1388d27c6fc8408a55fa6
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-13 15:21:33 +0100

    home page now renders with reworked hero

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -5,13 +5,14 @@ import About from "../Components/Sections/About";
 import Projects from "../Components/Sections/Projects";
 import Contact from "../Components/Sections/Contact";
 import { projectsData } from "../Data/data";
+import { siteData } from "../Data/data";
 
 const HomePage = () => {
     const [setNavTransparency] = useOutletContext();
     useEffect(() => setNavTransparency(true), [setNavTransparency]);
     return (
         <main id="main">
-            <Hero/>
+            <Hero heroData={siteData.mainHero}/>
             <About themeColor='neon-green'/>
             <Projects themeColor='neon-red' projects={projectsData}/>
             <Contact themeColor='neon-blue'/>
