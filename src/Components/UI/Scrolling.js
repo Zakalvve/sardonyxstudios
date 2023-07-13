da9b5af4e0068c172087a208fa72991007d87b98
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-13 15:21:05 +0100

    added hover animations to scroll button

--- src/Components/UI/Scrolling.js
+++ src/Components/UI/Scrolling.js
@@ -1,14 +1,15 @@
 import { useEffect } from "react";
 import useScrollPosition from '@react-hook/window-scroll';
 import { icons } from '../../Assets/assets';
+import { motion } from "framer-motion";
 
 //displays a fixed button that allows the user to quickly scroll to the top of the document
 const ToTopButton = ({pageHeight, children, ...props}) => {
   const scrollPos = useScrollPosition(60);
-  const hidenButton = scrollPos <= 0; //|| scrollPos >= (pageHeight - window.innerHeight);
+  const hideButton = scrollPos <= 0; //|| scrollPos >= (pageHeight - window.innerHeight);
 
   return (
-    <button hidden={hidenButton} onClick={scrollToTop} className='fixed bottom-[15vh] right-[6vw] z-40 text-white/60' {...props}><icons.UpArrow className='w-10 h-10 xl:w-16 xl:h-16'/></button>
+    <motion.button opacity={0.6} whileHover={{scale: 1.3}} whileTap={{scale:0.8}} hidden={hideButton} onClick={scrollToTop} className='fixed bottom-[15vh] right-[6vw] z-40 text-white/60' {...props}><icons.UpArrow className='w-10 h-10 xl:w-16 xl:h-16'/></motion.button>
   )
 }
 
