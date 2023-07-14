14105cc49e8ede73ba31d55c0b25c5e7886dbd41
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-14 14:53:35 +0100

    changes to project data structure

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -6,9 +6,10 @@ import { Link } from "react-router-dom";
 import SkillsList from "./SkillsList";
 
 const ProjectCard = ({themeColor, project}) => {
+    const sources = project.image.sources.thumb ? project.image.sources.thumb : project.image.sources.landscape;
     return (
         <Link to={`/projects/${project.id}`} className='relative rounded-lg flex flex-col flex-1 border-space-400 border bg-space-500 shadow-card duration-300 md:hover:scale-[1.03] overflow-hidden md:basis-[30vw] 2xl:basis-[20vw]'>
-            <Picture sources={project.image.sources.landscape}>
+            <Picture sources={sources}>
                 <Image {...project.image.img} className='w-full aspect-video object-cover object-top border-b border-space-400'/>
             </Picture>
             <div className='grow py-6 px-4 md:px-8 flex flex-col gap-y-2 md:gap-y-4'>
@@ -16,9 +17,7 @@ const ProjectCard = ({themeColor, project}) => {
                 <Caption themeColor={themeColor} className='text-lg md:text-xl mb-0'>
                     {project.name}
                 </Caption>
-                <Paragraph className='max-w-full'>
-                    {project.description}
-                </Paragraph>
+                <project.CardDescription themeColor={themeColor} className='flex flex-col gap-4'/>
                 <div className='flex-1 flex flex-col-reverse mt-2'>
                     <SkillsList skills={project.skills}/>
                 </div>
