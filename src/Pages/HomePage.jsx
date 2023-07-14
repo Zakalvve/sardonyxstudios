4c21f9e7d157c444db422129290c88d36e190c7b
Fohristiwhirl-Skaffen-Amtiskaw <112807626+Fohristiwhirl-Skaffen-Amtiskaw@users.noreply.github.com>
2023-07-14 17:30:28 +0100

    exposed function to toggle the parallax on in browser

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useOutletContext } from "react-router-dom";
 import Hero from "../Components/Sections/Hero";
 import About from "../Components/Sections/About";
@@ -9,10 +9,16 @@ import { siteData } from "../Data/data";
 
 const HomePage = () => {
     const [setNavTransparency] = useOutletContext();
+    const [byPassParallax, setByPassParallax] = useState(true);
     useEffect(() => setNavTransparency(true), [setNavTransparency]);
+
+    window.toggleParallax = () => {
+        setByPassParallax(!byPassParallax);
+    }
+    
     return (
         <main id="main">
-            <Hero heroData={siteData.mainHero} byPassParallax={true}/>
+            <Hero heroData={siteData.mainHero} byPassParallax={byPassParallax}/>
             <About themeColor='neon-green'/>
             <Projects themeColor='neon-red' projects={projectsData}/>
             <Contact themeColor='neon-blue'/>
