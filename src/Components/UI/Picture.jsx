0369fc5c1068067bdecebcb55b9ed18381b71b3b
David Williams <david-zakalwe-williams@outlook.com>
2023-09-11 16:04:25 +0100

    fixed bug with Picture component

--- src/Components/UI/Picture.jsx
+++ src/Components/UI/Picture.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 const Picture = React.forwardRef(({sources, children, ...props}, ref) => {
+    console.log(sources);
     return (
-        <picture {...props}>
+        <picture {...props} ref={ref}>
             {sources && sources.map(({...source}, i) => (
                     <source key={i} {...source} />
                 ))}
